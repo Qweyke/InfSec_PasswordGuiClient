4fce686c91cca6984a347dc1dee7f1e1f2746e10
Qweyke <qweyke@gmail.com>
2025-03-22 12:21:43 +0500

    ui setup

--- src/loginprocessor.cpp
+++ src/loginprocessor.cpp
@@ -1,7 +1,5 @@
 #include "loginprocessor.h"
 
-// #define FILE_PATH "good"
-
 namespace {
 QString APP_PATH = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
 QString FILE_PATH = APP_PATH + "/PasswordGuiClient/db.json";
@@ -21,48 +19,25 @@ LoginProcessor::LoginProcessor()
         }
     }
 
-    if (dbFile.exists()) {
-        if (dbFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QByteArray dbFileData = dbFile.readAll();
-            qDebug() << "File has been read";
-            dbFile.close();
+    if (dbFile.exists())
+        readData();
 
-            QJsonDocument dbJsonDoc = QJsonDocument::fromJson(dbFileData);
-            if (dbJsonDoc.isNull()) {
-                qWarning() << "Parse json err";
-                return;
-            }
-
-            qDebug() << "Document parsed";
-            QJsonObject dbObject = dbJsonDoc.object();
-
-            if (dbObject.contains("users") && dbObject["users"].isObject()) {
-                dbUsers = dbObject;
-                qDebug() << "Users loaded";
-            } else {
-                qWarning() << "Unexpected file content";
-                return;
-            }
-        } else {
-            qWarning() << "Open file err";
-            return;
-        }
-
-    } else {
+    else {
         QJsonObject adminUser;
         adminUser["password"] = "";
-        adminUser["permissions"] = "admin";
+        adminUser["permissions"] = Permission::admin;
 
         // Adding admin to array of users
         dbUsers["admin"] = adminUser;
 
-        // dumpData();
+        dumpData();
     }
 }
 
 void LoginProcessor::dumpData()
 {
     if (dbFile.open(QIODevice::WriteOnly)) {
+        //Reformat json db obj to json doc
         QJsonDocument dbJsonDoc(dbUsers);
         dbFile.write(dbJsonDoc.toJson());
         dbFile.close();
@@ -71,3 +46,43 @@ void LoginProcessor::dumpData()
         return;
     }
 }
+
+void LoginProcessor::readData()
+{
+    if (dbFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QByteArray dbFileData = dbFile.readAll();
+        qDebug() << "File has been read";
+        dbFile.close();
+
+        QJsonDocument dbJsonDoc = QJsonDocument::fromJson(dbFileData);
+        if (dbJsonDoc.isNull()) {
+            qWarning() << "Parse json err";
+            return;
+        }
+
+        qDebug() << "Document parsed";
+        QJsonObject dbObject = dbJsonDoc.object();
+
+        if (dbObject.contains("users") && dbObject["users"].isObject()) {
+            dbUsers = dbObject;
+            qDebug() << "Users loaded";
+        } else {
+            qWarning() << "Unexpected file content";
+            return;
+        }
+    } else {
+        qWarning() << "Open file err";
+        return;
+    }
+}
+
+void LoginProcessor::registerUser(const QString &login, const QString &pass)
+{
+    if (!dbUsers.contains(login)) {
+        QJsonObject newUser;
+        newUser["password"] = pass;
+        newUser["permissions"] = Permission::user;
+    }
+}
+
+void LoginProcessor::changePass(const QString &oldPass, const QString &newPass) {}
--- src/loginprocessor.h
+++ src/loginprocessor.h
@@ -12,13 +12,18 @@
 
 class LoginProcessor
 {
+    enum Permission { admin = 0, user = 1 };
+
 public:
     LoginProcessor();
-    void dumpData();
+    void registerUser(const QString &login, const QString &pass);
+    void changePass(const QString &oldPass, const QString &newPass);
 
 private:
     QFile dbFile;
     QJsonObject dbUsers;
+    void readData();
+    void dumpData();
 };
 
 #endif // LOGINPROCESSOR_H
